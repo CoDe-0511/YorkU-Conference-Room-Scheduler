23f5003822eb2347f0f4f60ede83c530a409a2b1
CoDe-0511 <niuyutong200251@gmail.com>
2026-07-18 18:16:41 -0400

    Update RegisteredUser.java

--- src/main/java/ca/yorku/eecs3311/roomscheduler/RegisteredUser.java
+++ src/main/java/ca/yorku/eecs3311/roomscheduler/RegisteredUser.java
@@ -1,9 +1,18 @@
 package ca.yorku.eecs3311.roomscheduler;
-
+import java.math.BigDecimal;
+import java.util.Objects;
 public final class RegisteredUser extends Account {
     RegisteredUser(String id, String name, String email, String passwordHash,
                    boolean universityVerified, boolean active) {
         super(id, name, email, passwordHash, Role.REGISTERED_USER, universityVerified, active);
+        this.accountType = Objects.requireNonNull(accountType);
+    }
+    public AccountType accountType() {
+        return accountType;
+    }
+
+    public BigDecimal hourlyRate() {
+        return accountType.hourlyRate();
     }
 }
 
